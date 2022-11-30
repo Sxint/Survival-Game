eb1ef4fe1c51908c2442734bae9b3af71c6c4054
ilikeapple10 <90153447+ilikeapple10@users.noreply.github.com>
2022-11-30 12:19:25 -0500

    Resized Window, added a platform object and fixed jumping

---
+++ GameTemplate/Platform.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameTemplate
+{
+    public class Platform : DrawableGameComponent
+    {
+        public SpriteBatch spriteBatch { get; set; }
+        public Texture2D tex { get; set; }
+        public Vector2 position;
+        public string test { get; set; }
+
+        public Platform(Game game, SpriteBatch spriteBatch,
+            Texture2D tex, Vector2 Position) : base(game)
+        {
+            this.spriteBatch = spriteBatch;
+            this.tex = tex;
+            position = Position;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteFont regular = Game.Content.Load<SpriteFont>("fonts/regularFont");
+            spriteBatch.Begin();
+            spriteBatch.Draw(tex, position, Color.White);
+            spriteBatch.DrawString(regular, position.Y.ToString(), new Vector2(position.X, position.Y), Color.White);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+        //hitbox
+        public Rectangle getBounds()
+        {
+            return new Rectangle((int)position.X, (int)position.Y, tex.Width, tex.Height);
+        }
+    }
+}
