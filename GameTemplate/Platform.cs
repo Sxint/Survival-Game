39a452c72ce78a494dbd603563448536cf0b81e9
ilikeapple10 <90153447+ilikeapple10@users.noreply.github.com>
2022-12-05 11:03:27 -0500

    updated collision (still broken)

--- GameTemplate/Platform.cs
+++ GameTemplate/Platform.cs
@@ -13,11 +13,12 @@ namespace GameTemplate
     {
         public SpriteBatch spriteBatch { get; set; }
         public Texture2D tex { get; set; }
+
         public Vector2 position;
         public string test { get; set; }
 
         public Platform(Game game, SpriteBatch spriteBatch,
-            Texture2D tex, Vector2 Position) : base(game)
+        Texture2D tex, Vector2 Position) : base(game)
         {
             this.spriteBatch = spriteBatch;
             this.tex = tex;
@@ -29,6 +30,10 @@ namespace GameTemplate
             SpriteFont regular = Game.Content.Load<SpriteFont>("fonts/regularFont");
             spriteBatch.Begin();
             spriteBatch.Draw(tex, position, Color.White);
+            spriteBatch.Draw(tex, getTopBounds(), Color.Gray);
+            spriteBatch.Draw(tex, getRightBounds(), Color.Gray);
+            spriteBatch.Draw(tex, getLeftBounds(), Color.Gray);
+
             spriteBatch.DrawString(regular, position.Y.ToString(), new Vector2(position.X, position.Y), Color.White);
             spriteBatch.End();
             base.Draw(gameTime);
@@ -36,15 +41,15 @@ namespace GameTemplate
         //hitbox
         public Rectangle getLeftBounds()
         {
-            return new Rectangle((int)position.X + tex.Width, (int)position.Y + 20, 0, tex.Height);
+            return new Rectangle((int)position.X + tex.Width - 5, (int)position.Y + 3, tex.Width - (tex.Width - 5), tex.Height);
         }
         public Rectangle getRightBounds()
         {
-            return new Rectangle((int)position.X, (int)position.Y + 20, 0, tex.Height);
+            return new Rectangle((int)position.X, (int)position.Y + 3, tex.Width - (tex.Width - 5), tex.Height);
         }
         public Rectangle getTopBounds()
         {
-            return new Rectangle((int)position.X + 5, (int)position.Y - 20, tex.Width - 50, tex.Height);
+            return new Rectangle((int)position.X, (int)position.Y, tex.Width, tex.Height - (tex.Height - 5));
         }
     }
 }
